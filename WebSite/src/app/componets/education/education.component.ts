fd4d874d95ba3db41a2c48eb11699b8f1b5788b9
a-ture <a.ture@studenti.unisa.it>
2023-03-26 12:05:37 +0200

    Education

--- WebSite/src/app/componets/education/education.component.ts
+++ WebSite/src/app/componets/education/education.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Education} from "../../models/education";
 
 @Component({
   selector: 'app-education',
@@ -7,6 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducationComponent implements OnInit {
 
+  educationList: Education[] = [
+    {
+      institute: "University of Pennsylvania's",
+      course: 'MBA',
+      duration: '2000-2002',
+      score: '80%',
+    },
+    {
+      institute: 'Stanford University',
+      course: 'M.S in Material Science',
+      duration: '1993-1995',
+      score: '75%',
+    },
+    {
+      institute: 'IIT (Madras)',
+      course: 'BTech in Metallurgical Engineering',
+      duration: '1989-1993',
+      score: '85%',
+    },
+    {
+      institute: 'Vana Vani School',
+      course: 'HSC',
+      duration: '1988-1989',
+      score: '80%',
+    },
+    {
+      institute: 'Vana Vani School',
+      course: 'SSC',
+      duration: '1986-1988',
+      score: '90%',
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
