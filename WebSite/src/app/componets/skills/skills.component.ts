1f8463db54b3c3b920282d374a5e1f008ae05e69
a-ture <a.ture@studenti.unisa.it>
2023-03-26 12:13:00 +0200

    Skill

--- WebSite/src/app/componets/skills/skills.component.ts
+++ WebSite/src/app/componets/skills/skills.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Skill} from "../../models/education";
 
 @Component({
   selector: 'app-skills',
@@ -6,7 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
-
+  skills: Skill[] = [
+    {
+      name: 'Angular, Angular Material',
+      level: 'Expert',
+      rating: 85,
+    },
+    {
+      name: 'ASP.NET Core, EF Core',
+      level: 'Intermidiate',
+      rating: 70,
+    },
+    {
+      name: 'HTML, CSS, JS',
+      level: 'Expert',
+      rating: 90,
+    },
+    {
+      name: 'JAVA',
+      level: 'Expert',
+      rating: 80,
+    },
+    {
+      name: 'C++',
+      level: 'Intermidiate',
+      rating: 70,
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
