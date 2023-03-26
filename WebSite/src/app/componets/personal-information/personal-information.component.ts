dbfde2f151d01c4dc28281a6ebb812f3d4280bc3
a-ture <a.ture@studenti.unisa.it>
2023-03-26 12:08:01 +0200

    PersonalInformation

--- WebSite/src/app/componets/personal-information/personal-information.component.ts
+++ WebSite/src/app/componets/personal-information/personal-information.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-personal-information',
@@ -6,8 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personal-information.component.css']
 })
 export class PersonalInformationComponent implements OnInit {
+  myData: string[][] = [
+    ['Name', 'Sundar Pichai'],
+    ['DOB', '10/06/1972'],
+    ['Work Exp', '25 Years'],
+    ['Education', 'M.S (1995)'],
+    ['Interests', 'Cricket'],
+  ];
 
-  constructor() { }
+  aboutMe: string[] = [
+    'Hi, I am a Software Engineer with 25 years of experience in software industry.',
+    'Worked as Head of Product Management in Google for various technologies (Google Search Engine, Google Drive, Chrome Browser).',
+    'Delivered all product withing deadlines. Always eager to learn new technologies.',
+    'Currently, working as CEO of Google Alphabet',
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
